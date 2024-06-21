c34c0d5ddd3cc293c34e24752a2fb74f55289fa0
DavidIsrael <davidisrael.rj@gmail.com>
2024-06-21 20:52:51 -0300

    Componete Card

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ function App() {
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className='linha'>        
-        <Card />
+        <Card title='Card1' red>X</Card>
+        <Card title='Card2' green>X</Card>
+      </div>
+      <div className='linha'>        
+        <Card title='Card3' blue>Y</Card>
+        <Card title='Card4' purple>Y</Card>
       </div>
     </div>
   );
