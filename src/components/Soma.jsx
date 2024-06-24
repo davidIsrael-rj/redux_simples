10dacb8926e43759787e2dadcb914db49fee3acb
DavidIsrael <davidisrael.rj@gmail.com>
2024-06-24 20:18:35 -0300

    Conectando o componente Soma com Redux

--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -1,20 +1,26 @@
 import React from "react"
-
+import { connect } from "react-redux"
 import Card from "./Card"
 
-function Soma(props) {    
-
+function Soma (props) {
+ 
+    const {min, max} = props
     return (
         <Card title="Soma de Números" blue>
             <div className="Intervalo">
                 <span>
                     <span>Resultado:</span>
-                    <strong>{10}</strong>
+                    <strong>{(max + min)}</strong>
                 </span>
             </div>
         </Card>
     )
 }
 
-
-export default Soma
+function mapStateToProp(state){
+    return{
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+export default connect(mapStateToProp)(Soma)
