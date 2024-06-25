5dcdfc18ff1eb8b14e009e2dde74fc5dad880851
DavidIsrael <davidisrael.rj@gmail.com>
2024-06-25 19:00:23 -0300

    Invocando Action Creator #03

--- src/store/actions/numeros.js
+++ src/store/actions/numeros.js
@@ -1,7 +1,16 @@
+import { type } from "@testing-library/user-event/dist/type"
+
 // Action Creator
 export function alterarNumeroMinimo(novoNumero){
     return{
         type: 'NUM_MIN_ALTERADO',
+        payload: novoNumero
+    }
+}
+
+export function alterarNumeroMaximo(novoNumero){
+    return{
+        type: 'NUM_MAX_ALTERADO',
         payload: novoNumero
     }
 }
